907e3875b5a48ea76e2a05f0ca4328aac69b0fdf
masterUNG <phrombutr@gmail.com>
2016-01-26 16:40:14 +0700

    button Controller finish

--- app/src/main/java/appbsru/supergolf/healthrecord/MeetDoctorSetup.java
+++ app/src/main/java/appbsru/supergolf/healthrecord/MeetDoctorSetup.java
@@ -2,12 +2,49 @@ package appbsru.supergolf.healthrecord;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MeetDoctorSetup extends AppCompatActivity {
+
+    //Explicit
+    private TextView showTimeTextView;
+    private Button setupTimeButton;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meet_doctor_setup);
-    }
-}
+
+        //Bind Widget
+        bindWidget();
+
+        //buttonController
+        buttonController();
+
+    }   // Main Method
+
+    private void buttonController() {
+
+        setupTimeButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+
+            }   // event
+        });
+
+    }   // buttonController
+
+    private void bindWidget() {
+
+        showTimeTextView = (TextView) findViewById(R.id.textView23);
+        setupTimeButton = (Button) findViewById(R.id.button3);
+
+    }   // bindWidget
+
+
+}    // Main Class
